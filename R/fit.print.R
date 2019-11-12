db037b0953ef529cd1c4764bb86ee1edc591f5e0
iatgen <iatgen.team@gmail.com>
2019-11-12 15:55:32 -0800

    Update fit.print.R
    Fixed issue where p-value for chi-square was for the wrong model

--- R/fit.print.R
+++ R/fit.print.R
@@ -6,7 +6,7 @@ fit.print <- function(x){
 
   pval.round <- function(p){
     if (p < .001) {p <- "< .001"}
-    if (p >= .001 & p < .005) {p <- paste0("= ",substr(format(round(p, 2), nsmall=2), 2, 5))}
+    if (p >= .001 & p < .005) {p <- paste0("= ",substr(format(round(p, 3), nsmall=3), 2, 5))}
     if (p >= .005) {p <- paste0("= ",substr(format(round(p, 2), nsmall=2), 2, 4))}
     return(p)
   }
@@ -17,7 +17,7 @@ fit.print <- function(x){
   ll <- sums$RMSEA_90CI_LB
   ul <- sums$RMSEA_90CI_UB
 
-  out <- paste0("X2(",sums$ChiSqM_DF,") = ", round.twodecimal(sums$ChiSqM_Value), ", p ", pval.round(sums$ChiSqBaseline_PValue),
+  out <- paste0("X2(",sums$ChiSqM_DF,") = ", round.twodecimal(sums$ChiSqM_Value), ", p ", pval.round(sums$ChiSqM_PValue),
                 ", SRMR = ", round.twodecimal(sums$SRMR),
                 ", CFI = ",round.twodecimal(sums$CFI),
                 # ", TLI = ", round.twodecimal(sums$TLI),
