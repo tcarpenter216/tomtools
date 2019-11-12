bda411390da12c01282bbb47904c03f673b13a35
iatgen <iatgen.team@gmail.com>
2019-11-12 13:56:12 -0800

    Update fit.print.R
    Added 90% CI for RMSEA, removed TLI (redundant to CFI)

--- R/fit.print.R
+++ R/fit.print.R
@@ -1,34 +1,35 @@
 # r function that displays model fit from an Mplus object
 
-
 fit.print <- function(x){
-
+  
   sums <- x$summaries
-
+  
   pval.round <- function(p){
     if (p < .001) {p <- "< .001"}
-    if (p >= .001 & p < .005) {p <- paste("= ",substr(as.character(round(p, 3)), 2, 5), sep="")}
-    if (p >= .005) {p <- paste("= ",substr(as.character(round(p, 2)), 2, 4), sep="")}
+    if (p >= .001 & p < .005) {p <- paste0("= ",substr(format(round(p, 2), nsmall=2), 2, 5))}
+    if (p >= .005) {p <- paste0("= ",substr(format(round(p, 2), nsmall=2), 2, 4))}
     return(p)
   }
-
-  round.twodecimal <- function(stat){
-    stat <- round(stat, 2)
-    dec.location <- str_locate(stat, "\\.")[1]
-    if(is.na(dec.location)){
-      stat <- paste(as.character(stat), ".00", sep="")
-      return(stat)
-    }
-    len <- str_length(stat)
-    num.dec <- len-dec.location
-    if(num.dec==1) {stat <- paste(as.character(stat), "0", sep="")}
-    stat <- as.character(stat)
-    return(stat)
-  }
-
-  out <- paste("X2(",sums$ChiSqM_DF,") = ",round.twodecimal(sums$ChiSqM_Value), ", p ", pval.round(sums$ChiSqBaseline_PValue), ", CFI = ",
-               round.twodecimal(sums$CFI), ", TLI = ", round.twodecimal(sums$TLI), ", RMSEA = ", round.twodecimal(sums$RMSEA_Estimate),
-               ", SRMR = ", round.twodecimal(sums$SRMR), sep="")
-
+  
+  round.twodecimal <- function(stat){format(round(stat, 2), nsmall=2)}
+  
+  #RMSEA CI
+  ll <- sums$RMSEA_90CI_LB
+  ul <- sums$RMSEA_90CI_UB
+  CI <- paste0("RMSEA CI90 [", 
+               format(round(ll, 3), nsmall=3) %>% substring(., 2),
+               ", ", 
+               format(round(ul, 3), nsmall=3) %>% substring(., 2),
+               "]")
+  
+  
+  out <- paste0("X2(",sums$ChiSqM_DF,") = ", round.twodecimal(sums$ChiSqM_Value), ", p ", pval.round(sums$ChiSqBaseline_PValue),
+                ", SRMR = ", round.twodecimal(sums$SRMR),
+                ", CFI = ",round.twodecimal(sums$CFI),
+                # ", TLI = ", round.twodecimal(sums$TLI),
+                ", RMSEA = ", round.twodecimal(sums$RMSEA_Estimate),
+                ", RMSEA CI90 [", substring(format(round(ll, 3), nsmall=3), 2), ", ", substring(format(round(ul, 3), nsmall=3), 2),
+                "]")
+  
   return(out)
 }
